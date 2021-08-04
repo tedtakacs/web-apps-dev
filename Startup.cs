c18b71161383b127d81104fdddefaa6904c5c124
Ted Takacs <tedtakacs@users.noreply.github.com>
2021-08-04 10:19:41 -0400

    updated messages

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace web_apps_dev
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello Azure!");
+                    await context.Response.WriteAsync("Hello Azure!!!");
                 });
             });
         }
